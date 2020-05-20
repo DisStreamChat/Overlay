e79eccf6b7209dafff733134987f76c46bacc96d
David Snyder <davidgraygs4@gmail.com>
2020-05-19 21:36:26 -0400

    updated to be similar to chat app

--- src/App.js
+++ src/App.js
@@ -1,105 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import firebase from "./firebase"
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase";
+import "./App.css";
 
-import openSocket from 'socket.io-client';
-import Message from './components/Message';
-import Header from './components/Header';
+import openSocket from "socket.io-client";
+import Message from "./components/Message";
+
+import { AppContext } from "./contexts/AppContext";
 
 // URLSearchParams
 function App() {
-  const [userId, setUserId] = useState("")
-  const [error, setError] = useState("")
-  const [streamerInfo, setStreamerInfo] = useState()
-  const [socket, setSocket] = useState()
-  const [messages, setMessages] = useState([])
-  const [loaded, setLoaded] = useState(false) 
+	const [userId, setUserId] = useState("");
+	const [error, setError] = useState("");
+	const [streamerInfo, setStreamerInfo] = useState();
+	const [socket, setSocket] = useState();
+	const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    if(loaded){
-      console.log("hi");
-      
-      localStorage.setItem("messages", JSON.stringify(messages))
-    }
-  }, [messages, loaded])
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.href);
+		if (urlParams.has("id")) {
+			setUserId(urlParams.get("id"));
+		} else {
+			setError({
+				code: "404",
+				message: "missing user id",
+			});
+		}
+	}, []);
 
-  useEffect(() => {
-    (async () => {
-      const localMessages = JSON.parse(localStorage.getItem("messages"))
-      if(localMessages){
-        await setMessages(localMessages)
-      }
-      setLoaded(true)
-    })()
-  }, [])
+	useEffect(() => {
+		setSocket(openSocket("http://localhost:3200"));
+	}, []);
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href)
-    if(urlParams.has("id")){
-      setUserId(urlParams.get("id"))
-    }else{
-      setError({
-        code: "404",
-        message: "missing user id"
-      })
-    }
-  }, [])
+	useEffect(() => {
+		if (socket) {
+			socket.on("chatmessage", (msg) => {
+				setMessages((m) => [...m.slice(m.length - 100, m.length), msg]);
+			});
+		}
+	}, [socket]);
 
-  useEffect(() => {
-    setSocket(openSocket('http://localhost:3200'))
-  },[])
+	useEffect(() => {
+		if (userId?.length > 0) {
+			(async () => {
+				const db = firebase.firestore();
+				const unsubscribe = db
+					.collection("Streamers")
+					.doc(userId)
+					.onSnapshot(snapshot => {
+						setStreamerInfo(snapshot.data());
+					});
+				return () => unsubscribe();
+			})();
+		}
+	}, [userId]);
 
-  useEffect(() => {
-    if(socket){
-      socket.on("chatmessage", msg => {
-        setMessages(m => [...m, msg].slice(0, 100))
-      })
-    }
-  }, [socket])
+	useEffect(() => {
+		if (streamerInfo) {
+			// send infoString to backend with sockets, to get proper socket connection
+			if (socket) {
+				socket.emit("addme", streamerInfo);
+			}
+		}
+	}, [streamerInfo, socket]);
 
-  useEffect(() => {
-    if(userId?.length > 0){
-      (async () => {
-        const db = firebase.firestore()
-        const streamerInfo = await db.collection("Streamers").doc(userId).get()
-
-       setStreamerInfo(streamerInfo.data());
-      })();
-    }
-  }, [userId])
-
-  useEffect(() => {
-    if(streamerInfo){
-      // send infoString to backend with sockets, to get proper socket connection
-      if(socket){
-        socket.emit("addme", streamerInfo)
-      }
-    }
-  }, [streamerInfo, socket])
-
-  const removeMessage = id => {
-    setTimeout(() => {
-      const copy = [...messages].filter(m => m.uuid !== id)
-      console.log(copy)
-      setMessages(copy)
-    }, 800)
-  }  
-
-  console.log(messages);
-  
-
-  return (
-    <div className="App">
-      <Header setMessages={setMessages}/>
-      <div className="overlay-container">
-        <div className="overlay">
-          {messages.map(msg => (
-            <Message delete={removeMessage} key={msg.uuid} msg={msg} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<AppContext.Provider
+			value={{
+				userId,
+				setUserId,
+				streamerInfo,
+				setStreamerInfo,
+			}}
+		>
+			<div className="App">
+				<div className="overlay-container">
+					<div className="overlay">
+						{messages.map((msg) => (
+							<Message key={msg.uuid} msg={msg} />
+						))}
+					</div>
+				</div>
+			</div>
+		</AppContext.Provider>
+	);
 }
 
 export default App;
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,12 @@
-import React, { useState, useCallback} from "react";
+import React, { useState, useCallback, useEffect, useContext} from "react";
 import DOMPurify from "dompurify";
 import marked from "marked"
 import Avatar from "@material-ui/core/Avatar";
 import HighlightOffTwoToneIcon from '@material-ui/icons/HighlightOffTwoTone';
 import {CSSTransition} from "react-transition-group"
 import "./Message.css"
+import Tooltip from '@material-ui/core/Tooltip';
+import {AppContext} from "../contexts/AppContext"
 
 let renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
@@ -16,47 +18,48 @@ marked.setOptions({
     renderer: renderer
 });
 
+const discordLogo = "https://i.imgur.com/ZOKp8LH.png"
+const twitchLogo = "https://cdn.vox-cdn.com/thumbor/hSP3rKWFHC7hbbtpCp_DIKiRSDI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/2937002/twitch.0.jpg"
+
+const seconds = millis => millis*1000
+
 const Message = props => {
     const [active, setActive] = useState(true)
 
+    const [displayPlatform, setDisplayPlatform] = useState(true)
+    const { streamerInfo } = useContext(AppContext)
+
+    useEffect(() => {
+        setDisplayPlatform(streamerInfo?.displayPlatform)
+    }, [streamerInfo])
+
     const deleteMe = useCallback(() => {
-        props.delete(props.msg.uuid)
         setActive(false)
-    }, [props])
-    
+    }, [])
+
+    useEffect(() => {
+        if(streamerInfo) setTimeout(deleteMe, seconds(streamerInfo?.overlayTimeout))
+    }, [streamerInfo, deleteMe])
+
     return (
         <CSSTransition unmountOnExit in={active} timeout={700} classNames="my-node">
-            <div className={`message ${props.msg.platform}-message ${props.msg.messageId} ${!active && "fade-out"}`}>
+            <div className={`message ${displayPlatform === "full" && `${props.msg.platform}-message`} ${props.msg.messageId} ${!active && "fade-out"}`}>
                 <div className="name name-header">
                     <span className="name">
                         <div className="profile">
-                            <Avatar className="profile-pic" src={props.msg.avatar} alt={props.msg.displayName + " avatar"}/>
-                        </div>
-                        <span dangerouslySetInnerHTML={{
-                            __html: marked(DOMPurify.sanitize(props.msg.displayName, {
-                                FORBID_ATTR: [
-                                    "style",
-                                    "onerror",
-                                    "onload",
-                                    "width",
-                                    "height"
-                                ],
-                                FORBID_TAGS: [
-                                    "table",
-                                    "script",
-                                    "audio",
-                                    "video",
-                                    "style",
-                                    "iframe",
-                                    "textarea",
-                                    "input",
-                                    "form",
-                                ],
+                            <Avatar className="profile-pic" src={props.msg.avatar} alt=""/>
+                            {props.msg.badges.subscriber &&
+                                <Tooltip arrow title={props.msg.badges.subscriber.title} placement="top"><img className="sub-badge" src={props.msg.badges.subscriber.image} alt=""></img></Tooltip>
                             }
-                            ))}}>
+                            {Object.entries(props.msg.badges).map((badge, i) => {
+                                return badge[0] !== "subscriber" ? <Tooltip arrow title={badge[1].title} placement="top"><img src={badge[1].image} alt="" className={`chat-badge badge-${i}`}></img></Tooltip> : <></>
+                            })}
+                        </div>
+                        <span>
+                            {props.msg.displayName}
                         </span>
+                        {displayPlatform === "medium" && <Tooltip title={props.msg.platform} placement="top" arrow><img width="20" src={props.msg.platform === "discord" ? discordLogo : twitchLogo} alt="platform" className={"chat-badge " + props.msg.platform} /></Tooltip>}
                     </span>
-                    <HighlightOffTwoToneIcon onClick={deleteMe} className="exit-button" />
                 </div>
                 <div className="msg-body" dangerouslySetInnerHTML={{
                     __html: marked(DOMPurify.sanitize(props.msg.body,{
